ae9f6298dba232e35b92dd92b5f6294a23a78f77
JLiu <107297778+lifeoak0@users.noreply.github.com>
2024-01-31 15:28:51 +0800

    Update CNNsModelGEN_Predic.py

--- CNNsModelGEN_Predic.py
+++ CNNsModelGEN_Predic.py
@@ -1,4 +1,4 @@
-import torch
+import torch  #环境准备：导入必要的库和模块 包括torch torch.nn torch.optim torchvision
 import torch.nn as nn
 import torch.optim as optim
 from torchvision import transforms, datasets
@@ -8,18 +8,18 @@ from PIL import Image
 # 检查是否有可用的 GPU，如果没有则使用 CPU
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-# CNN 模型定义
-class SimpleCNN(nn.Module):
+# CNN 模型定义   
+class SimpleCNN(nn.Module):  #SimpleCNN继承自NN.module并定义了一个点单的卷积神经网络。网络包含三个卷积层（conv1 conv2 conv3）每层后面跟着ReLU激活函数和最大池化层
     def __init__(self):
         super(SimpleCNN, self).__init__()
         self.conv1 = nn.Conv2d(3, 32, kernel_size=3, padding=1)
         self.conv2 = nn.Conv2d(32, 64, kernel_size=3, padding=1)
         self.conv3 = nn.Conv2d(64, 128, kernel_size=3, padding=1)
-        self.pool = nn.MaxPool2d(2, 2)
+        self.pool = nn.MaxPool2d(2, 2)   
         self.relu = nn.ReLU()
         # 根据卷积层输出和图像尺寸计算全连接层的输入尺寸
         num_features = 25 * 25 * 128
-        self.fc1 = nn.Linear(num_features, 128)
+        self.fc1 = nn.Linear(num_features, 128) #然后是两个全连接层（fully connected layer）最后一个全连接层后面跟着sigmoid激活函数，用于图像二分类问题（dog or cat）
         self.fc2 = nn.Linear(128, 1)
         self.sigmoid = nn.Sigmoid()
 
@@ -34,18 +34,18 @@ class SimpleCNN(nn.Module):
 
 model = SimpleCNN().to(device)
 
-# 损失函数和优化器
+# 损失函数和优化器（定义损失函数，使用二元交叉熵损失函数BCEWithLogitsLoss）
 criterion = nn.BCEWithLogitsLoss()
-optimizer = optim.Adam(model.parameters(), lr=0.001)
+optimizer = optim.Adam(model.parameters(), lr=0.001)  #选择Adam优器，学习率设为0.001 
 
-# 数据转换：统一尺寸、转换为张量、归一化
+# 数据转换：统一尺寸、转换为张量、归一化（使用transform定义一个转化序列，包括调整图像大学，转化为张量tensor，并归一化Normalized）
 transform = transforms.Compose([
     transforms.Resize((200, 200)),
     transforms.ToTensor(),
     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
 ])
 
-# 加载数据集
+# 加载数据集（带入本地需要用于训练的文件， train函数用于模型的训练，对每个批次的图像进行处理，计算损失，反向传播，并后患模型参数）
 train_data = datasets.ImageFolder(r'T:\neuraldata\DOGCATarchive\training_set', transform=transform)
 train_loader = DataLoader(train_data, batch_size=32, shuffle=True)
 
